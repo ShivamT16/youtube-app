248bc15e99fd531a33ac782c9dc2aef23bdf0953
Shivam Tripathi <109875608+ShivamT16@users.noreply.github.com>
2025-03-06 16:25:53 +0530

    fix: update LiveChat dispatch logic and improve header styles

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const MY_YOUTUBE_API_KEY = 'AIzaSyDUWIOYFuKpWJ1oMPgUXC8NxVbmwPMmEiU'
+const MY_YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY
 export const YOUTUBE_VIDEOS_API= 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key='+ MY_YOUTUBE_API_KEY; 
 export const YOUTUBE_SEARCH_API= 'http://suggestqueries.google.com/complete/search?client=chrome&ds=yt&q='
 export const YOUTUBE_SEARCH_RESULTS_API='https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&key='+MY_YOUTUBE_API_KEY+'&q='
